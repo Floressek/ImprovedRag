52e3bedd7cb5464b54446fa1d00a9591c835134b
Floressek <szymon.florek@student.wat.edu.pl>
2025-11-08 20:51:51 +0100

    Add CoVeStatus enumeration for tracking verification states

---
+++ src/ragx/retrieval/cove/constants/cove_status.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class CoVeStatus(str, Enum):
+    ALL_VERIFIED = "all_verified"
+    MISSING_EVIDENCE = "missing_evidence"
+    LOW_CONFIDENCE = "low_confidence"
+    CRITICAL_FAILURE = "critical_failure"
